f53d59facbcbcf683c37719e38a425ca8978cad2
cyanorussel <anmolrajverma@gmail.com>
2025-06-13 13:04:32 +0530

    socket

--- socket/socket.js
+++ socket/socket.js
@@ -8,8 +8,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
-		methods: ["GET", "POST"],
+		origin: [
+			"http://localhost:3000",
+			"https://nitards-frontend.vercel.app"
+		],
+		credentials: true
 	},
 });
 
