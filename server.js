f4e8659150698e7020c349c479e42305a319c623
cyanorussel <anmolrajverma@gmail.com>
2025-06-13 13:27:42 +0530

    1

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 connectDB();
 job.start();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -62,4 +62,4 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server started at :${PORT}`));
